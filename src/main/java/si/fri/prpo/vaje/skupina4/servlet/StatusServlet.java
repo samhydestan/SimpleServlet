e4b19c550f306caa17e1cad5f0c5e1d92ae65190
Gašper <gg7889@student.uni-lj.si>
2019-10-23 18:01:55 +0200

    Dodan izpis configa

--- src/main/java/si/fri/prpo/vaje/skupina4/servlet/StatusServlet.java
+++ src/main/java/si/fri/prpo/vaje/skupina4/servlet/StatusServlet.java
@@ -1,15 +1,22 @@
 package si.fri.prpo.vaje.skupina4.servlet;
+
+import com.kumuluz.ee.configuration.utils.ConfigurationUtil;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/servlet")
 public class StatusServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.printf("I've been all around the grlobe. Globe.\n-Sam Hyde\n");
+        ConfigurationUtil conf=ConfigurationUtil.getInstance();
+        PrintWriter pw=resp.getWriter();
+        pw.printf("I've been all around the grlobe. Globe.\n-Sam Hyde\n\n\n");
+        pw.printf("Name: "+conf.get("kumuluzee.name").get()+"\nVersion: "+conf.get("kumuluzee.version").get()+"\nRun env. name: "+conf.get("kumuluzee.env.name").get());
+        pw.close();
     }
 }
